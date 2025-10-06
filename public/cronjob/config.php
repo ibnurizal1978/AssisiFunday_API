85285ce314ccdcd61e02ab4f31d6be6951965dc5
Ibnu R <ibnurizal@gmail.com>
2025-10-06 10:21:50 +0700

    Delete public/cronjob/config.php

--- public/cronjob/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$base_url         = 'https://assisifunday.sg/';
-$db_server        = '10.148.0.5';
-$db_user          = 'assisi';
-$db_password      = 'Database@123';
-$db_name          = 'db_assisi';
-$conn 			  = new mysqli($db_server,$db_user,$db_password,$db_name) or die (mysqli_error($conn));
-
-?>
